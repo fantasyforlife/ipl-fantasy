e4f4cfd6b985b244ee4965ce6509e8bdfbb9f46c
saurav <saurav@saurav-VirtualBox.(none)>
2015-03-31 00:45:05 +0530

    ICC Cricket Fantsay

--- iplh2h/settings.py
+++ iplh2h/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'head2head',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,8 +83,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+
+# to push on heroku uncomment this line.
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- iplh2h/urls.py
+++ iplh2h/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
+
+import head2head.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'iplh2h.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', head2head.views.index, name='index'),
+    # url(r'^db', head2head.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 )
